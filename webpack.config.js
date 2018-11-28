04bd27d412b37453395a1bcbc3f2e93208d41969
xuzicheng <xzc@cohl.com>
2018-11-28 23:20:53 +0800

    简单配置webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',    // 入口文件
+    output: {
+        filename: 'bundle.js',      // 打包后的文件名称
+        path: path.resolve('dist')  // 打包后的目录，必须是绝对路径
+    }
+}
